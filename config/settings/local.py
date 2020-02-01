ebb0fadc3859f996cad21a28feb856f8816aa6d0
황 재원 <ka8590@naver.com>
2020-02-01 15:03:54 +0000

    django rest before start2

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="faOyNKIAyzPYsjPRvZNfYN9weEpv6Zl2Cm6p7yM8lCng1cOxkzKwdTLiUZNjeaJM",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","123"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 # CACHES
 # ------------------------------------------------------------------------------
